b2549ede6ff532530d278851925dc2249334abae
NeoCat98 <00076017@uca.edu.sv>
2020-05-17 18:01:49 -0600

    tarea completa

---
+++ src/main/java/com/uca/capas/controller/MainController.java
@@ -0,0 +1,63 @@
+package com.uca.capas.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.uca.capas.dao.EstudianteDAO;
+import com.uca.capas.domain.Estudiante;
+
+@Controller
+public class MainController {
+	@Autowired
+	private EstudianteDAO estudianteDAO;
+	
+	@RequestMapping("/inicio")
+	public ModelAndView inicio() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("estudiante",new Estudiante());
+		mav.setViewName("index");
+		
+		return mav;
+	}
+	
+	
+	@RequestMapping("/listado")
+	public ModelAndView initMain() {
+		ModelAndView mav = new ModelAndView();
+		List<Estudiante> estudiantes = null;
+		try {
+			estudiantes = estudianteDAO.findAll();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		mav.addObject("estudiantes", estudiantes);
+		mav.setViewName("listado");
+
+		return mav;
+	}
+	
+	@RequestMapping("/insertarEstudiante")
+	public ModelAndView insert(@Valid @ModelAttribute Estudiante estudiante, BindingResult result) {
+		ModelAndView mav = new ModelAndView();
+		System.out.println(result.toString());
+		if (result.hasErrors()) {
+			mav.setViewName("index");
+		} else {
+			mav.addObject("estudiante",new Estudiante());
+			mav.setViewName("index");
+			estudianteDAO.insert(estudiante);
+		}
+		
+		return mav;
+	}
+}
